4966d1eef8ff9735143b9c665c9bfc6beea3fc88
柯小乐 <2244951869@qq.com>
2021-01-06 00:12:37 +0800

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main()
+{
+	int a,b;
+	float x,y;
+	char c1,c2;
+	scanf("a=%d b=%d",&a,&b);
+	//其中scanf中输入的数据类型 需 与输出的数据类型一致
+	scanf("%f %e",&x,&y);
+	scanf("c1=%c c2=%c",&c1,&c2);
+	printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c\n",a,b,x,y,c1,c2);
+	return 0;
+}
