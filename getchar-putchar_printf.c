d1492e6d15ed45c512ef1b13f6676dcf453af596
柯小乐 <2244951869@qq.com>
2021-01-08 18:43:58 +0800

    Add files via upload

--- getchar-putchar_printf.c
+++ getchar-putchar_printf.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
-int main()
-{
-	char c1,c2;
-	int a,b;
-	c1=97;
-	c2=98;
-	//printf("c1=%c,c2=%c\n",c1,c2);
-	//printf("c1=%d,c2=%d\n",c1,c2);
-	
-	a=getchar();
-	b=getchar();
-	putchar(a);
-	//putchar(b);
+int main(){
+    int a=2,b=4,i=8,a3,b3,c3;
+	int a1='m8',b1='89',c1,c2; //赋值为多个字符时,只认最后一个
+    //printf("AAAAA=%-d FFFF=%-d" , a,b);
 	printf("\n");
-	printf("%c",b);
+
+	//一种键盘输出char类型 只输出一个字符
+	putchar(a1);putchar('\t');
+	putchar(b1); //除非putchar(89) 才会输出字符,变量值为89无法输出对应字符,只输出9
+	//putchar(a1);putchar(b1);
+	putchar('\n');
+
+	//i++:先运算再赋值 ++i:先赋值再运算 依次向下
+	//所以1=8 2=9 3=10 4=10 ...
+	//printf("%d\n",i++); //1
+	//printf("%d\n",i); //2
+	//printf("%d\n",++i); //3
+	//printf("%d\n",i); //4
+	//printf("%d\n",i++);
+	//printf("%d\n",++i);
+	//printf("\n");
+
+	//从右至左 并且是并行关系 所以先运算再赋值无效
+	//所以 结果是 9 9 9 9 8
+	//printf("%d\n%d\n%d\n%d\n%d\n",i,i,i,++i,i);
+
+	//一种键盘输入char类型 只接收一个字符
+	printf("PLEASE IPUUT:");
+	c2 = getchar();
+	c1 = getchar();
+	putchar(c1);
+	putchar(c2);
+	//printf("ggg:%c",getchar());
+	//putchar(c1);
 	printf("\n");
-	//return 0;
+
+	//更优的键盘输入
+	//sizeof: 显示字节长度
+	//printf("input a3 b3 c3:");
+	//scanf("%d,%d,%d",&a3, &b3, &c3);
+	//printf("%d %% - %d - %d",sizeof(a3),b3,c3);
+	printf("\n");
 }
-	
+
+
