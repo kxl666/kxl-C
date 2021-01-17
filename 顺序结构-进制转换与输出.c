985c33c2b5e630e50561b5161cf8578a95be7e7f
柯小乐 <2244951869@qq.com>
2021-01-17 20:10:43 +0800

    Update 顺序结构-进制转换与输出.c

--- 顺序结构-进制转换与输出.c
+++ 顺序结构-进制转换与输出.c
@@ -9,5 +9,12 @@ int main()
     printf("a=%hd, b=%d, c=%ld\n", a, b, c);  //以十进制形式输出
     printf("a=%hx, b=%x, c=%lx\n", a, b, c);  //以十六进制形式输出（字母小写）
     printf("a=%hX, b=%X, c=%lX\n", a, b, c);  //以十六进制形式输出（字母大写）
+    //输出格式加上#加以区分
+    /*
+    printf("a=%#ho, b=%#o, c=%#lo\n", a, b, c);  //以八进制形似输出
+    printf("a=%hd, b=%d, c=%ld\n", a, b, c);     //以十进制形式输出
+    printf("a=%#hx, b=%#x, c=%#lx\n", a, b, c);  //以十六进制形式输出（字母小写）
+    printf("a=%#hX, b=%#X, c=%#lX\n", a, b, c);  //以十六进制形式输出（字母大写）
+    */
     return 0;
 }
