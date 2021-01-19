0081ac56b5628b3722732329d4db6a585280760c
柯小乐 <2244951869@qq.com>
2021-01-19 13:48:32 +0800

    Update 综合01-特忆用法.c

--- 综合01-特忆用法.c
+++ 综合01-特忆用法.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+//用到数组
 int main()
 {
 	char arr[100] = {0};
 	gets(arr);
 	int i = 0;
-	int flag = 0;
+	int flag = 0; //一开始赋值为0 要注意！
 	while(arr[i] != '0')
 	{
 		if(arr[i] == 'A')
 		{
-			flag++;
+			flag++; //02在循环体内部对同一个值++或-- 这个思维要记忆
 		}
 		else if(arr[i] == 'B')
 		{
-			flag--;
+			flag--; //03
 		}	
 		i++;
 	}
-		if(flag>0)
+		if(flag>0) //04 最后判断flag值是否为0
 			printf("A");
 		else if(flag<0)
 			printf("B");
