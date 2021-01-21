41430c655a01482c999091ee1366f5ba749f7c7f
柯小乐 <2244951869@qq.com>
2021-01-21 16:10:22 +0800

    Create 综合04-判断输入值是否在给定范围中.c

---
+++ 综合04-判断输入值是否在给定范围中.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	char ch = 0;
+	char arr[] = "AEIOUaeiou"; //01数组思想
+	while((ch=getchar()) != EOF)
+	{
+		int i = 0;
+		for(i=0; i<10; i++)
+		{
+			if(ch == arr[i]) //02
+			{
+				printf("Vowel\n");
+				break;
+			}
+		}
+		if(i == 10)
+			printf("Consonant\n");
+		getchar();//去除每个字符后的\n
+	}
+	return 0; }
