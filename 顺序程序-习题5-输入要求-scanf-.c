97a0748afb9615ca4b1cab3a0387d12afd845415
柯小乐 <2244951869@qq.com>
2021-01-21 11:19:44 +0800

    Update 顺序程序-习题5-输入要求-scanf-.c

--- 顺序程序-习题5-输入要求-scanf-.c
+++ 顺序程序-习题5-输入要求-scanf-.c
@@ -5,11 +5,12 @@ int main()
 	float x,y;
 	char c1,c2;
 	char url[30]; //此时没有立即给字符串url赋值,所以它不能自动计算长度,所以不能url[]
-	scanf("%s", url); //可以加&也可以不加&
+	//scanf("%s", url); //可以加&也可以不加&
 	scanf("a=%d b=%d",&a,&b);
 	//其中scanf中输入的数据类型 需 与输出的数据类型一致
 	scanf("%f %e",&x,&y);
-	scanf("c1=%c c2=%c",&c1,&c2);
-	printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c,url=%s\n",a,b,x,y,c1,c2,url);
+	scanf("%c %c",&c1,&c2);
+	//printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c,url=%s\n",a,b,x,y,c1,c2,url);
+	printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c\n",a,b,x,y,c1,c2);
 	return 0;
 }
