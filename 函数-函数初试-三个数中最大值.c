a86dd7fcfb61d8e4ec9bab6aced42b4623237fe0
柯小乐 <2244951869@qq.com>
2021-01-22 10:39:53 +0800

    Update 函数-函数初试-三个数中最大值.c

--- 函数-函数初试-三个数中最大值.c
+++ 函数-函数初试-三个数中最大值.c
@@ -1,3 +1,4 @@
+/*写法01调用多个函数解决问题
 #include <stdio.h>
 int main()
 {
@@ -24,5 +25,26 @@ int max02(int x,int y)
 	ma=(x>y)?x:y;
 	//mb=(max>z)?max:z;
 	return(ma);
+}*/
+
+//写法02 只用一个函数,此场景中这种更简洁
+#include <stdio.h>
+int main()
+{
+	int max02(int x,int y);
+	int x,y,z;
+	scanf("%d %d %d",&x,&y,&z);
+	printf("max=%d\n",(max02(x,y)>z)?max02(x,y):z);
+	return 0;
 }
+
+int max02(int x,int y)
+{
+	int ma;
+	ma=(x>y)?x:y;
+	//mb=(max>z)?max:z;
+	return(ma);
+}
+
+
 
