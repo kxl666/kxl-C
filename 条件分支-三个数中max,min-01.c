a9394eeb5d2f171a7fecb5eba83583973b264376
柯小乐 <2244951869@qq.com>
2021-01-31 21:39:52 +0800

    Update 条件分支-三个数中max,min-01.c

--- 条件分支-三个数中max,min-01.c
+++ 条件分支-三个数中max,min-01.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 int main()
 {
-
+	
 	int a,b,c,max,min;
 	scanf("%d,%d,%d",&a,&b,&c);
 	if ( a > b )
 	{	
-	 max = a;
-	 min = b;
-	 if ( a > c  &&  b > c )
-	 min = c;
-	 else
-	 min = b;
-	 if ( a < c )
-	 max = c;  
+		max = a;
+		min = b;
+		if ( a > c  &&  b > c )
+			min = c;
+		else
+			min = b;
+		if ( a < c )
+			max = c;  
 	}
 	else
 	{
-	 if ( b < c )
-	 {
-	  max = c;
-	  min = a;
-	 }
-	 else
-	 {
-	  if ( a > c )
-	  {
-	  max = b;
-	  min = c;
-	  }
-	  else
-	  {
-	  max=b;
-          min=a;
-	  }
-	 }
+		if ( b < c )
+		{
+			max = c;
+			min = a;
+		}
+		else
+		{
+			if ( a > c )
+			{
+				max = b;
+				min = c;
+			}
+			else
+			{
+				max=b;
+				min=a;
+			}
+		}
 	}
 	
 	
