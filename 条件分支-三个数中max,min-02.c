825355fef103cfb05a636482dfd1156c5b671f25
柯小乐 <2244951869@qq.com>
2021-01-31 21:49:30 +0800

    Update 条件分支-三个数中max,min-02.c

--- 条件分支-三个数中max,min-02.c
+++ 条件分支-三个数中max,min-02.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-//所以说用else if 比较笨但简单，把所有情况列出来即可
 int main()
 {
-
+	
 	int a,b,c,max,min;
 	scanf("%d,%d,%d",&a,&b,&c);
 	if ( a > b && a > c && b > c )
 	{
-	max = a;
-	min = c;
+		max = a;
+		min = c;
 	}
 	else if ( a > b && a > c && b < c )
 	{
-	max = a;
-	min = b;
+		max = a;
+		min = b;
 	}
 	else if ( a > b && a < c && b < c )
 	{
-	max = c;
-	min = b;
+		max = c;
+		min = b;
 	}
 	else if ( a < b && a > c && b > c )	
 	{
-	max = b;
-	min = c;
+		max = b;
+		min = c;
 	}
 	else if ( a < b && a < c && b < c )
 	{
-	max = c;
-	min = a;
+		max = c;
+		min = a;
 	}
 	else if ( a < b && a < c && b > c )
 	{
-	max = b;
-	min = a;	
+		max = b;
+		min = a;	
 	}
 	printf("max=%d,min=%d",max,min);
 	putchar('\n');
