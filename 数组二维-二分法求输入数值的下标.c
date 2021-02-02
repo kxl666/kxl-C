ff0e23d69eb135adea1b7cbd1cd34d4119064f52
柯小乐 <2244951869@qq.com>
2021-02-02 10:35:45 +0800

    Update 数组二维-二分法求输入数值的下标.c

--- 数组二维-二分法求输入数值的下标.c
+++ 数组二维-二分法求输入数值的下标.c
@@ -5,12 +5,12 @@ int main()
 {
 	static int a[M]={-12,0,6,16,23,56,80,100,110,115}; //一个程序在编译运行的时候，普通变量是存放在栈区，
 	//此数组为有序排列                                 //我们还会学习堆，而这个static会使得变量存放在data
-                                                   	   //我们整个内存主要分为四大区:CODE，DATA，STACK ,HEAP
+	//我们整个内存主要分为四大区:CODE，DATA，STACK ,HEAP
 	int n,low,mid,high,found;
 	low=0;
 	high=M-1;
 	found=0;
-	printf("please input a num:\n");
+	printf("please input a num:");
 #if (0)
 	do
 	{
@@ -21,7 +21,8 @@ int main()
 	
 	while(scanf("%d",&n) !=1) //EOF:非法输入 0:没有输入
 	{
-		printf("illegal input\n");
+		printf("illegal input:");
+		getchar();
 	}
 	while(low<=high)
 	{
