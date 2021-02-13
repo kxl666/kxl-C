b26dbf7f5fe00c1f33638ce75e20094677cdc725
柯小乐 <2244951869@qq.com>
2021-02-13 17:28:03 +0800

    Update 条件分支-习题4-三数中最大数03.c

--- 条件分支-习题4-三数中最大数03.c
+++ 条件分支-习题4-三数中最大数03.c
@@ -7,7 +7,8 @@ int main()
   int a,b,c,temp,max;
   printf("请输入三个整数:");
   scanf("%d,%d,%d",&a,&b,&c);
-  temp = ( a > b ) ? a:b;
-  max = ( temp > c ) ? temp:c;
+  max = ( a > b ) ? a:b;
+  max = ( max > c ) ? max:c;
+  //printf("三个数的最大值是%d\n",( (( a > b ) ? a:b) > c ) ? (( a > b ) ? a:b):c);
   printf("三个数的最大值是%d\n",max);
 }
