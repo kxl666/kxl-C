4547f7a5a274218dffe92856dfad7d1f2bd35763
柯小乐 <2244951869@qq.com>
2021-02-24 13:40:20 +0800

    Update 函数-习题17-递归整数转换成字符串.c

--- 函数-习题17-递归整数转换成字符串.c
+++ 函数-习题17-递归整数转换成字符串.c
@@ -11,7 +11,7 @@ int main()
 	printf("\n");
 }
 
-void convert(int n)
+void convert(int n) //此函数注意，例如输入345 因为是递归所以puchar先执行最后一圈再执行第二圈，再执行第一圈!!! 不然像循环结构-【反向输出%5D】.c 会输出543了 
 {
 	int i;
 	if ((i=n/10)!=0)
