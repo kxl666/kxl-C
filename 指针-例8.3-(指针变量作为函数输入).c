c1d160c485924db9387c6f47135358acb0fadff3
柯小乐 <2244951869@qq.com>
2021-03-05 14:15:54 +0800

    Create 指针-例8.3-(指针变量作为函数输入).c

---
+++ 指针-例8.3-(指针变量作为函数输入).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//改变其他函数中指针变量的值
+
+int main()
+{
+	void swap(int *p1,int *p2);
+	int a,b;
+	int *point1,*point2;
+	printf("please input a and b-");
+	scanf("%d,%d",&a,&b);
+	point1=&a;
+	point2=&b;
+	if (a<b)
+		swap(point1,point2);
+	printf("max=%d,min=%d\n",a,b);
+}
+
+void swap(int *p1,int *p2)
+{
+	int temp;
+	temp=*p1;
+	*p1=*p2;
+	*p2=temp;
+}
