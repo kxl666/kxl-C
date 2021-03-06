5ac9a0540c314dd5cd6ab9b38ff25f93492baa5a
柯小乐 <2244951869@qq.com>
2021-03-06 20:11:23 +0800

    Update 指针-(通过指针引用数组)例8.8-(用数组名作函数参数).c

--- 指针-(通过指针引用数组)例8.8-(用数组名作函数参数).c
+++ 指针-(通过指针引用数组)例8.8-(用数组名作函数参数).c
@@ -2,27 +2,32 @@
 
 int main()
 {
-	void inv(int x[],int n);
-	int i,a[10]={3,7,9,11,0,6,7,5,4,2};
+	void inv(int *x,int n);
+	int i,a[10]={3,7,9,11,0,6,7,5,4,2},*p=a;
 	printf("The original array-\n");
-	for(i=0;i<10;i++)
-		printf("%d",a[i]);
+	for(i=0;i<10;i++,p++)
+		printf("%d",*p);
 	printf("\n");
-	inv(a,10);
+	p=a;
+	inv(p,10);//inv(a,10);
 	printf("The array has been inverted-\n");
-	for(i=0;i<10;i++)
-		printf("%d",a[i]);
+	//for(i=0;i<10;i++)
+	//	printf("%d",a[i]);
+	for(;p<a+10;p++)
+		printf("%d",*p);
 	printf("\n");
 }
 
-void inv(int x[],int n)
+void inv(int *x,int n)
 {
-	int temp,i,j,m=(n-1)/2;
-	for(i=0;i<=m;i++)
+	int *p,temp,*i,*j,m=(n-1)/2;
+	i=x;
+	j=x+n-1;
+	p=x+m;
+	for(;i<=p;i++,j--)
 	{
-		j=n-1-i;
-		temp=x[i];
-		x[i]=x[j];
-		x[j]=temp;
+		temp=*i;
+		*i=*j;
+		*j=temp;
 	}
 }
