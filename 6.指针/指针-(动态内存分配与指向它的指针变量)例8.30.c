14819675f2781f0c3ec7cb68733b8f34b437e060
柯小乐 <2244951869@qq.com>
2021-03-09 13:17:50 +0800

    Update 指针-(动态内存分配与指向它的指针变量)例8.30.c

--- 6.指针/指针-(动态内存分配与指向它的指针变量)例8.30.c
+++ 6.指针/指针-(动态内存分配与指向它的指针变量)例8.30.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//01 怎样建立内存的动态分配
 int main()
 {	
 	void check(int *);
@@ -20,3 +21,23 @@ void check(int *p)
 			printf("%d ",p[i]);
 		printf("\n");
 }
+
+//02 void指针类型
+/*
+int a=3;
+//定义a为整型变量
+int *p1= &a;
+//p1指向int型变量
+char *p2;
+//p2指向char型变量
+void *p3;
+//p3为无类型指针变量(基类型为void型)
+p3=(void *)pl;
+//将p1的值转换为void #类型,然后赋值给p3
+p2= (chart *)p3; .
+//将p3的值转换为char.类型,然后赋值给p2
+print("%d",*pl);
+//合法,输出a的值
+p3=&a;printf("%d",*p3);
+//错误,p3是无指向的,不能指向a
+*/
