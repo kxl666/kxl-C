cdfc13161d1e691146480999714ecae635ca3882
柯小乐 <2244951869@qq.com>
2021-03-11 13:23:09 +0800

    Create 21.用指向指针的指针的方法对n个整数排序并输出。要求将排序单独写成一个函 数。 n个整数在主函数中输人,最后在主函数中输出.c

---
+++ 6.指针/指针课后习题/21.用指向指针的指针的方法对n个整数排序并输出。要求将排序单独写成一个函 数。 n个整数在主函数中输人,最后在主函数中输出.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	void sort(int **p,int n);
+	int i,n,data[20],**p,*pstr[20];
+	printf("Input n-\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		pstr[i]=&data[i];
+	printf("input %d interger number-\n",n);
+	for(i=0;i<n;i++)
+		scanf("%d",pstr[i]);
+	p=pstr;
+	sort(p,n);
+	printf("Now,the sequence is-\n");
+	for(i=0;i<n;i++)
+		printf("%d ",*pstr[i]);
+	printf("\n");
+}
+
+void sort(int **p,int n)
+{
+	int i,j,*temp;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if (**(p+i)>**(p+j))
+			{
+				temp=*(p+i);
+				*(p+i)=*(p+j);
+				*(p+j)=temp;
+			}
+		}
+	}
+}
