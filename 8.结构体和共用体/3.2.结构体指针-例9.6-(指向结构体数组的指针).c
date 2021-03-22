1bafd5227d753f6b9b793c3a60d22f4609a4960c
柯小乐 <2244951869@qq.com>
2021-03-22 17:12:34 +0800

    Update 3.2.结构体指针-例9.6-(指向结构体数组的指针).c

--- 8.结构体和共用体/3.2.结构体指针-例9.6-(指向结构体数组的指针).c
+++ 8.结构体和共用体/3.2.结构体指针-例9.6-(指向结构体数组的指针).c
@@ -16,3 +16,8 @@ int main()
 	for(p=stu;p<stu+3;p++)
 		printf("%5d %-20s %2c %4d\n",p->num,p->name,p->sex,p->age);
 }
+
+
+
+//p=(struct Student *)stu[0].name; 此时,p的值是stu[0]元素的name 成员的起始地址。可以用“printf("%s" ,p);”输出stu[0]中成员name的值。但是,p仍保持原来的类型。如果执行“printf("%s" ,p+1);",则
+//会输出stu[1]中name的值。执行p十十时,p的值增加了结构体struct Student的长度。
