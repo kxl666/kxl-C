71e1b653551d759df003baa12ee5602724412964
柯小乐 <2244951869@qq.com>
2021-03-24 13:40:51 +0800

    Create 循环结构-输出图形2.c

---
+++ 3.循环结构/循环结构-输出图形2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=9;j++)
+			if (j==5||j==i-1||j==11-i)
+				printf("*");
+			else
+				printf(" ");
+			printf("\n");
+	}	
+}
+
+/*运行结果
+
+     *    
+*    *   *
+ *   *  *
+   * * *
+    ***
+
+*/
