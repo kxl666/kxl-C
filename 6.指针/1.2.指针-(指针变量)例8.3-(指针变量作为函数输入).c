e0668ed32afffcb2f3f94dd16ae45dde263acdb7
柯小乐 <2244951869@qq.com>
2021-03-25 22:48:30 +0800

    Update 1.2.指针-(指针变量)例8.3-(指针变量作为函数输入).c

--- 6.指针/1.2.指针-(指针变量)例8.3-(指针变量作为函数输入).c
+++ 6.指针/1.2.指针-(指针变量)例8.3-(指针变量作为函数输入).c
@@ -2,7 +2,6 @@
 //改变其他函数中指针变量的值(所指代的变量名)
 //注意:函数的调用可以(而且只可以)得到一个返回值(即函数值),而使用指针变量作参数,可以得到多个变化了的值。如果不用指针变量是难以做到这一点的。要善于利用指针法。
 
-
 int main()
 {
 	void swap(int *p1,int *p2);
@@ -10,7 +9,7 @@ int main()
 	int *point1,*point2;
 	printf("please input a and b-");
 	scanf("%d,%d",&a,&b);
-	point1=&a;
+	point1=&a; //&*point1=&a, *&a=a, (*point1)++=a++
 	point2=&b;
 	if (a<b)
 		swap(point1,point2);
