f8e7ee21f053bee237f0cbadd0f48dfa4c3fb3fc
柯小乐 <2244951869@qq.com>
2021-03-25 16:03:22 +0800

    Update 2.3.2.指针-(通过指针引用数组)例8.13-(通过指针引用多维数组)-(指向多维数组元索的指针变量).c

--- 6.指针/2.3.2.指针-(通过指针引用数组)例8.13-(通过指针引用多维数组)-(指向多维数组元索的指针变量).c
+++ 6.指针/2.3.2.指针-(通过指针引用数组)例8.13-(通过指针引用多维数组)-(指向多维数组元索的指针变量).c
@@ -6,7 +6,7 @@ int main()
 	int b[4]={1,3,5,7};
 	int (*p)[4],i,j; //p指向包含4个整型元素的一维数组的指针变量
 	int (*q)[4]; //q指向包含4个元素的一维数组的指针变量
-	p=a;
+	p=a; //普通指针无法指定多维数组
 	q=&b; //表示q指向一维数组(行)
 	printf("please enter row and colum-");
 	scanf("%d,%d",&i,&j);
