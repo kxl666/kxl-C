9be49d31e4db2ce021f31ac292c979087a3c9a04
柯小乐 <2244951869@qq.com>
2021-03-26 18:41:58 +0800

    Create 7.1typedef.c

---
+++ 8.结构体和共用体/7.1typedef.c
@@ -0,0 +1,80 @@
+//用typedef声明新的类型名来代替已有的类型名
+
+//有些操作系统int型是2个字节，所以需要把所有int改为 long int.这个时候有typedef就好说啦!
+
+//01定义整型
+#include <stdio.h>
+
+typedef int In;
+
+int main()
+{	
+	In i=1;
+	int j=2;
+	printf("%d,%d\n",i,j);
+}
+
+
+//02定义结构体类型
+#include <stdio.h>
+
+typedef struct
+{
+	int month;
+	int day;
+	int year;
+}Date;
+
+int main()
+{	
+	Date date;
+	date.month=12;
+	date.day=23;
+	date.year=2021;
+
+	printf("%d-%d-%d\n",date.month,date.day,date.year);
+}
+
+
+//03定义整型数组
+#include <stdio.h>
+
+typedef int Num[5];
+
+int main()
+{	
+	Num a; //但是不能 a={1,2,3,4,5}
+	a[2]=2;
+	printf("%d\n",a[2]);
+}
+
+
+//04定义字符指针
+#include <stdio.h>
+
+typedef char * p;
+
+int main()
+{	
+	p p1;
+	p1="I love you";
+	printf("%s\n",p1);
+}
+
+
+//05定义指向函数的指针
+#include <stdio.h>
+typedef void (*P)();
+
+void fun();
+int main()
+{
+	P p1;
+	p1=fun;
+	(*p1)(); //=p1()
+}
+
+void fun()
+{
+	printf("I Lve You!\n");
+}
