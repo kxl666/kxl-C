9b5eba64fd91d53a2621a6f515b4545c2efb968f
柯小乐 <2244951869@qq.com>
2021-03-27 00:12:32 +0800

    Create 1.2.读写字符-例10.2.c

---
+++ 9.文件操作/1.2.读写字符-例10.2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE * in,* out;
+	char ch,infile[]="C:\\Users\\kxl\\Desktop\\kxl01.txt",outfile[]="C:\\Users\\kxl\\Desktop\\kxl02.txt";
+	/*
+	printf("please input infilename:");
+	scanf("%s",&infile);
+	printf("please input outfilename:");
+	scanf("%s",&outfile);*/
+	if((in=fopen(infile,"r"))==NULL)
+	{
+		printf("isn't exit %s\n",infile);
+		exit(0);
+	}
+	
+	if((out=fopen(outfile,"w"))==NULL)
+	{
+		printf("isn't exit %s\n",outfile);
+		exit(0);
+	}
+	
+	ch=fgetc(in);
+	while(!feof(in))
+	{
+		fputc(ch,out);
+		putchar(ch);
+		ch=fgetc(in);
+	}
+	putchar(10);
+	fclose(in);
+	fclose(out);
+}
