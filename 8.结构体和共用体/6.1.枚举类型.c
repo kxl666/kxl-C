8e3c454296e21501d2c490af81adc9ed8d7546bc
柯小乐 <2244951869@qq.com>
2021-03-29 22:38:53 +0800

    Create 6.1.枚举类型.c

---
+++ 8.结构体和共用体/6.1.枚举类型.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	enum Week {sun,mon,tue,wed,thu,fri,sat};
+	enum Week today;
+	struct tm *p;
+	time_t t;
+	
+	time(&t);
+	p=localtime(&t);
+	
+	today=p->tm_wday;
+	
+	switch(today)
+	{
+	case mon:
+	case tue:
+	case wed:
+	case thu:
+	case fri: printf("干活!\n"); break;
+	case sat:
+	case sun: printf("放假!\n"); break;
+	default: printf("errror!\n");				
+	}
+}
