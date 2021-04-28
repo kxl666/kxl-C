6d8527f155318cc8cffe5285072cf0ccf8a09345
柯小乐 <2244951869@qq.com>
2021-04-28 10:35:50 +0800

    Update 循环结构-goto.c

--- 3.循环结构/循环结构-goto.c
+++ 3.循环结构/循环结构-goto.c
@@ -6,12 +6,12 @@ int main()
 	int i,sum=0;
 	i=1;
 loop:	if(i<=100)
-	  {
-		sum+=i;
-		i++;
-		goto loop;
-	  }
-	printf("%d\n",sum);
+		{
+			sum+=i;
+			i++;
+			goto loop;
+		}
+		printf("%d\n",sum);
 }  
 
 #if( 0 )
