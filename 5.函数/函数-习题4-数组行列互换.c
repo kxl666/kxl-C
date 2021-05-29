0078fbcfcf75ceddabd271c74bfd5236573ce8f9
柯小乐 <2244951869@qq.com>
2021-05-29 19:20:21 +0800

    Update 函数-习题4-数组行列互换.c

--- 5.函数/函数-习题4-数组行列互换.c
+++ 5.函数/函数-习题4-数组行列互换.c
@@ -28,3 +28,40 @@ void f1(int number[][3])
 			printf("\n");
 		}
 }
+
+/*02不需要借助第二个数组，参考指针课后练习题9
+#include <stdio.h>
+
+int main()
+{
+	void f1(int number[][3]);
+	int num[3][3],i,j;
+	printf("please input a series of num-\n");
+	for(i=0;i<=2;i++)
+		for(j=0;j<=2;j++)
+			scanf("%d",&num[i][j]);
+		f1(num);
+}
+
+void f1(int number[][3])
+{
+	int i,j,nember[3][3],t;
+	for(i=0;i<=2;i++)
+		for(j=1;j<2;j++)
+		{
+			t=number[i][j];
+			number[i][j]=number[j][i];
+			number[j][i]=t;
+		}
+		
+		for(i=0;i<=2;i++)
+		{
+			printf("\n");
+			for(j=0;j<=2;j++)
+			{
+				printf("%d\t",number[i][j]);		
+			}
+			printf("\n");
+		}
+}
+*/
